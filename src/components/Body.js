6a66da08b2a6686c6b1da28b28e3d1afd54241f0
Vishnu Jangid <vishnu.j8503@gmail.com>
2024-02-11 16:55:02 +0530

    Readme Commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -17,7 +17,6 @@ const Body = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json=await data.json();
-    //optional chaining
     setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
   };
     useEffect(() => {
